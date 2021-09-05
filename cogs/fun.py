1e40bf1d41a8d0c39335f32367193959ce42e784
zutu <51734083+ZuTuu@users.noreply.github.com>
2021-09-06 01:50:17 +0700

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -3,11 +3,20 @@ from discord.ext import commands
 # import json
 
 class fun(commands.Cog):
-
+    """
+    Commands for fun
+    """
     def __init__(self, client):
         self.client = client
-    @commands.command()
-    async def ping(self, ctx):
+    @commands.command(name="ping", brief='Ping')
+    async def _ping(self, ctx):
+        """
+        Melihat ping bot
+        Contoh: >ping
+        """
+        # `Command`: download(url:required)
+        # `NOTE`: file size can't exceed 8MB, otherwise it will fail to upload and cause error
+        # """
         await ctx.send(f'Indihome sekarang sedang {round(self.client.latency * 100)}ms!')
 
 def setup(client):
