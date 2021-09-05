9c9715cfc6437bb7a78dfd3b2942012db6d2f274
zutu <51734083+ZuTuu@users.noreply.github.com>
2021-09-06 01:49:59 +0700

    Update mods.py

--- cogs/mods.py
+++ cogs/mods.py
@@ -5,16 +5,49 @@ from discord.ext.commands import MissingPermissions, has_permissions, bot_has_pe
 import json
 
 class mods(commands.Cog):
+    """
+    Commands untuk admin/mod
+    """
     def __init__(self, client):
         self.client = client
-        
-    @commands.command(name="purge")
-    async def _purge(self, ctx, *, amount=0):
-        if(amount < 1):
+
+    # @commands.command(name="cek")
+    # async def _ceke(self, ctx):
+    #     print(ctx.guild.id)
+    @commands.is_owner()
+    @commands.command()
+    async def unload(ctx, ext):
+        client.unload_extension(f"cogs.{ext}")
+        await ctx.send(f"unloaded {ext}")
+
+    @commands.is_owner()
+    @commands.command()
+    async def load(ctx, ext):
+        client.load_extension(f"cogs.{ext}")
+        await ctx.send(f"loaded {ext}")
+
+    @commands.is_owner()
+    @commands.command()
+    async def reload(ctx, ext):
+        client.unload_extension(f"cogs.{ext}")
+        client.load_extension(f"cogs.{ext}")
+        await ctx.send(f"reloaded {ext}")
+
+    @commands.command(name="purge", brief='Menghapus messages')
+    async def _purge(self, ctx, *, jumlah):
+        """
+        Menghapus beberapa messages sebelumnya
+        Contoh: >purge 3
+        """
+        # `Command`: download(url:required)
+        # `NOTE`: file size can't exceed 8MB, otherwise it will fail to upload and cause error
+        # """
+        jumlah = 0
+        if(jumlah < 1):
             await ctx.send(f"Tolong ketik jumlah message(s) yang ingin di hapus! <@{ctx.message.author.id}>")
         else:
-            await ctx.channel.purge(limit=amount + 1 )
-            embedVar = discord.Embed(title=f'Telah dihapus {amount} message', colour=discord.Colour.from_rgb(64, 239, 255))
+            await ctx.channel.purge(limit=jumlah + 1 )
+            embedVar = discord.Embed(title=f'Telah dihapus {jumlah} message', colour=discord.Colour.from_rgb(64, 239, 255))
             await ctx.send(embed=embedVar, delete_after=5.0)
             return
             # else:
@@ -22,9 +55,17 @@ class mods(commands.Cog):
             #     await ctx.send(embed=embedVar2, delete_after=5.0)
 
     @commands.bot_has_permissions(kick_members=True)
-    @commands.command(name="kick")
+    @commands.command(name="kick", brief='Mengeluarkan member')
     @commands.has_permissions(kick_members=True)
-    async def _kick(self, ctx, member : discord.Member, *, reason="Gaada"):
+    async def _kick(self, ctx, member : discord.Member, *, alasan):
+        """
+        Mengeluarkan member melalui command
+        Contoh: >kick @zutu#4866
+        """
+        # `Command`: download(url:required)
+        # `NOTE`: file size can't exceed 8MB, otherwise it will fail to upload and cause error
+        # """
+        alasan = "Gaada"
         bot_member = ctx.guild.get_member(self.client.user.id)
         if(bot_member.top_role <= member.top_role):
             await ctx.send("Saya tidak bisa mengeluarkan orang dengan role yang lebih tinggi")
@@ -47,16 +88,24 @@ class mods(commands.Cog):
                     except asyncio.TimeoutError:
                         await ctx.send(f"Waktu terlalu lama untuk mereact! <@{ctx.author.id}>")
                     else:
-                        await ctx.send(f"<@{member.id}> telah dikeluarkan dari server ini\nAlasan: {reason}")
-                        await member.kick(reason=reason)
+                        await ctx.send(f"<@{member.id}> telah dikeluarkan dari server ini\nAlasan: {alasan}")
+                        await member.kick(reason=alasan)
                 else:
-                    await ctx.send(f"<@{member.id}> telah dikeluarkan dari server ini\nAlasan: {reason}")
-                    await member.kick(reason=reason)
+                    await ctx.send(f"<@{member.id}> telah dikeluarkan dari server ini\nAlasan: {alasan}")
+                    await member.kick(reason=alasan)
 
     @commands.bot_has_permissions(ban_members=True)
-    @commands.command(name="ban")
+    @commands.command(name="ban", brief='Mengusir member')
     @commands.has_permissions(ban_members=True)
-    async def _ban(self, ctx, member : discord.Member, *, reason="Gaada"):
+    async def _ban(self, ctx, member : discord.Member, *, alasan):
+        """
+        Mengeluarkan member secara permanen melalui command
+        Contoh: >ban @zutu#4866
+        """
+        # `Command`: download(url:required)
+        # `NOTE`: file size can't exceed 8MB, otherwise it will fail to upload and cause error
+        # """
+        alasan = "Gaada"
         bot_member = ctx.guild.get_member(self.client.user.id)
         if(bot_member.top_role <= member.top_role):
             await ctx.send("Saya tidak bisa mengeluarkan orang dengan role yang lebih tinggi")
@@ -79,16 +128,27 @@ class mods(commands.Cog):
                     except asyncio.TimeoutError:
                         await ctx.send(f"Waktu terlalu lama untuk mereact! <@{ctx.author.id}>")
                     else:
-                        await ctx.send(f"<@{member.id}> telah dikeluarkan dari server ini\nAlasan: {reason}\nhttps://tenor.com/view/rip-pack-bozo-dead-gif-20309754")
-                        await member.ban(reason=reason)
+                        await ctx.send(f"<@{member.id}> telah dikeluarkan dari server ini\nAlasan: {alasan}\nhttps://tenor.com/view/rip-pack-bozo-dead-gif-20309754")
+                        await member.ban(reason=alasan)
                 else:
-                    await ctx.send(f"<@{member.id}> telah dikeluarkan dari server ini\nAlasan: {reason}\nhttps://tenor.com/view/rip-pack-bozo-dead-gif-20309754")
-                    await member.ban(reason=reason)
+                    await ctx.send(f"<@{member.id}> telah dikeluarkan dari server ini\nAlasan: {alasan}\nhttps://tenor.com/view/rip-pack-bozo-dead-gif-20309754")
+                    await member.ban(reason=alasan)
 
     @commands.bot_has_permissions(ban_members=True)
-    @commands.command(name="unban")
+    @commands.command(name="unban", brief='Menghilangkan ban member')
     @commands.has_permissions(ban_members=True)
     async def _unban(self, ctx, *, member):
+        """
+        Menghilangkan ban member sehingga member bisa join kembali
+        Contoh: >unban zutu#4866
+
+        # `Command`: download(url:required)
+         Catatan: username tidak memakai tanda "@"
+         """
+        # """
+        # if(member.id == ctx.message.author.id):
+        #     await ctx.send(f"Sumpah lu nape dah <@{ctx.author.id}>")
+        #     return
         banned_users = await ctx.guild.bans()
         member_name, member_tag = member.split('#')
 
